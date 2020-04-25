aaadd62fbac8e2007b95d0acc4364ad10bd97ae3
svalverd <40702702+svalverd@users.noreply.github.com>
2020-04-25 03:36:15 -0400

    Add files via upload

---
+++ static/logic.js
@@ -0,0 +1,5 @@
+var gl = L.mapboxGL({
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    accessToken: 'not-needed',
+    style: 'https://api.maptiler.com/maps/3d26a01e-f289-425f-b403-d7737e1ae42f/style.json?key=G18kR4B5cKkYaH1F1cW3'
+  }).addTo(map)
